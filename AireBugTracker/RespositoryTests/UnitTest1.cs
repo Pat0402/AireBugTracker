b517aa16f7c40ae75bbc90111ef59c4ae2f86ace
Pat Davy <pdavy0402@gmail.com>
2022-09-28 17:15:31 +0100

    Create solution structure with required projects

---
+++ AireBugTracker/RespositoryTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RespositoryTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
