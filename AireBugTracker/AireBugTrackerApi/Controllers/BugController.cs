72ecb8f2659dd8c391cdb9ccacd20a114aad2d2b
Pat Davy <pdavy0402@gmail.com>
2022-10-01 16:37:42 +0100

    Setup DI and connection string for iniitial docker test

--- AireBugTracker/AireBugTrackerApi/Controllers/BugController.cs
+++ AireBugTracker/AireBugTrackerApi/Controllers/BugController.cs
@@ -1,4 +1,6 @@
+using DatabaseContext.Models;
 using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
 
 namespace AireBugTrackerApi.Controllers
 {
@@ -7,10 +9,27 @@ namespace AireBugTrackerApi.Controllers
     public class BugController : ControllerBase
     {
         private readonly ILogger<BugController> _logger;
+        private IBugService _bugService;
 
-        public BugController(ILogger<BugController> logger)
+        public BugController(ILogger<BugController> logger, IBugService bugService)
         {
             _logger = logger;
+            _bugService = bugService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await _bugService.CreateAsync(new Bug
+            {
+                Id = 1,
+                Title = "First Bug",
+                Details = "These are the details of the first bug",
+                OpenedDate = DateTimeOffset.UtcNow,
+                IsOpen = true
+            });
+            var bugs = await _bugService.GetAll();
+            return Ok(bugs);
         }
     }
 }
--- AireBugTracker/AireBugTrackerApi/Program.cs
+++ AireBugTracker/AireBugTrackerApi/Program.cs
@@ -1,22 +1,31 @@
+using DatabaseContext;
+using Microsoft.Extensions.Configuration;
+using Repositories.Interfaces;
+using Repositories.Respositories;
+using Services.Interfaces;
+using Services.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Dependency Injection
+var connectionString = builder.Configuration.GetConnectionString("BugTrackerContext");
+builder.Services.AddScoped<BugTrackerContext>(s => new BugTrackerContext(connectionString));
+builder.Services.AddScoped<IBugRepository, BugRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IBugService, BugService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
